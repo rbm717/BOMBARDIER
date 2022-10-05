967037f652ce9d14c3a8dd60609a3e87db4abced
rbm717 <47369758+rbm717@users.noreply.github.com>
2022-10-05 15:10:11 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+title = "HI";
+
+description = `
+`;
+
+characters = [];
+
+const G ={
+   WIDTH: 150,
+   HEIGHT: 150, 
+};
+
+//Actual code of the game
+options = {
+    viewSize: {x: G.WIDTH, y:G.HEIGHT},
+};
+
+function update() {
+    if (!ticks) {
+    }
+
+    if(input.isJustPressed){
+
+    }
+
+
+}
+
+addEventListener("load", onLoad);
